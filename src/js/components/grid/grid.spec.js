fd8abc8436f9f0bf2f0edd6d60127496b19bba72
StefGeraets <stef.geraets@gmail.com>
2021-08-25 20:10:53 +0200

    Add package to make DOM tests more readable

--- src/js/components/grid/grid.spec.js
+++ src/js/components/grid/grid.spec.js
@@ -12,11 +12,11 @@ describe("renderGrid", () => {
 
   it("should render grid cells by specified amount", () => {
     const gridDom = document.querySelector("#grid");
-    expect(gridDom).toBe(gridDom);
+    expect(gridDom).toBeEmptyDOMElement();
 
     renderGrid(gridDom, 4, 4);
     expect(gridDom.childElementCount).toBe(16);
-    expect(gridDom.firstChild.classList.contains("grid-item-0-0")).toBe(true);
-    expect(gridDom.lastChild.classList.contains("grid-item-3-3")).toBe(true);
+    expect(gridDom.firstChild).toHaveClass("grid-item-0-0");
+    expect(gridDom.lastChild).toHaveClass("grid-item-3-3");
   });
 });
