1000be744a4ae041a7083e199acf629184164d02
Richard Köster <rhm.koster@gmail.com>
2021-09-01 23:35:10 +0200

    Calculate game times according to percentages

--- src/js/data/state.js
+++ src/js/data/state.js
@@ -16,4 +16,59 @@ export const time = {
   isPlaying: false,
   interval: "",
   elapsed: 0,
+  total: 3 * 60 * 1000, // 3 minutes for testing purposes,
+  percentiles: [
+    {
+      name: "beforeStart",
+      percentage: 10,
+    },
+    {
+      name: "firstZoneStart",
+      percentage: 21,
+    },
+    {
+      name: "firstZoneCountdown",
+      percentage: 25,
+    },
+    {
+      name: "firstZoneShrink",
+      percentage: 30,
+    },
+    {
+      name: "secondZoneStart",
+      percentage: 41,
+    },
+    {
+      name: "secondZoneCountdown",
+      percentage: 45,
+    },
+    {
+      name: "secondZoneShrink",
+      percentage: 50,
+    },
+    {
+      name: "thirdZoneStart",
+      percentage: 57,
+    },
+    {
+      name: "thirdZoneCountdown",
+      percentage: 61,
+    },
+    {
+      name: "thirdZoneShrink",
+      percentage: 66,
+    },
+    {
+      name: "fourthZoneStart",
+      percentage: 73,
+    },
+    {
+      name: "fourthZoneCountdown",
+      percentage: 77,
+    },
+    {
+      name: "fourthZoneShrink",
+      percentage: 82,
+    },
+  ],
 };
--- src/js/events/loop.js
+++ src/js/events/loop.js
@@ -3,9 +3,24 @@ import * as UI from "@app/ui/dm";
 
 const increaseTick = () => (time.elapsed += time.tick);
 
+const calculateGameTimes = () =>
+  time.percentiles.map((percentageZone) => ({
+    time: time.total * (percentageZone.percentage / 100),
+    name: percentageZone.name,
+  }));
+
 const loop = () => {
   increaseTick();
-  console.log(time.elapsed);
+  const timezones = calculateGameTimes();
+  const event = timezones.find((element) => element.time === time.elapsed);
+
+  if (event !== undefined) {
+    triggerTimeEvent(event);
+  }
+
+  if (time.elapsed >= time.total) {
+    clearInterval(time.interval);
+  }
 };
 
 export const togglePlayPause = () => {
@@ -17,5 +32,37 @@ export const togglePlayPause = () => {
     time.interval = setInterval(loop, time.tick);
   } else {
     clearInterval(time.interval);
+  }
+};
+
+const triggerTimeEvent = (event) => {
+  console.log(event);
+  switch (event) {
+    case "beforeStart":
+      break;
+    case "firstZoneStart":
+      break;
+    case "firstZoneCountdown":
+      break;
+    case "firstZoneShrink":
+      break;
+    case "secondZoneStart":
+      break;
+    case "secondZoneCountdown":
+      break;
+    case "secondZoneShrink":
+      break;
+    case "thirdZoneStart":
+      break;
+    case "thirdZoneCountdown":
+      break;
+    case "thirdZoneShrink":
+      break;
+    case "fourthZoneStart":
+      break;
+    case "fourthZoneCountdown":
+      break;
+    case "fourthZoneShrink":
+      break;
   }
 };
